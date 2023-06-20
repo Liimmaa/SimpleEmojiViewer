46c0978607ffabf6cb725fb6511ebce707d0dce0
Liimmaa <halimammegwa@gmail.com>
2023-06-20 03:18:07 +0100

    Initial Commit

---
+++ SimpleEmojiViewer/SimpleEmojiViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleEmojiViewerApp.swift
+//  SimpleEmojiViewer
+//
+//  Created by Chioma Amanda Mmegwa  on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleEmojiViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
